1c0e3b1e195d3223d78d1485a0575dca6510ee53
Slifer-The-Sky-Dragon <aghaei.alirezza@gmail.com>
2021-04-05 14:19:35 +0430

    Initialization2

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 // #include "parser.hpp"
 // #include "user.hpp"
 #include <vector>
-
+#include "user.hpp"
 #include "socket.hpp"
 
 using namespace std;
@@ -18,9 +18,20 @@ int main(){
 // 	for(int i = 0 ; i < users.size() ; i++)
 // 		cout << users[i].username << ' ' << users[i].password << ' ' << users[i].is_admin << ' ' << users[i].download_size << endl;
 
+	int server_command_port = 8000;
+	int server_data_port = 8001;
+
+	User admin;
+	admin.username = "Ali";
+	admin.password = "1234";
+	admin.is_admin = 1;
+	admin.download_size = 100000;
+
     Socket s(SOCK_STREAM);
     s.bindSock(INADDR_LOOPBACK, 10000);
     s.setOpt(SO_REUSEADDR);
     cout << "socket created and bound successfully" << '\n';
     cout << "fd: " << s.fd() << ", addr: " << s.addr().first << ", " << s.addr().second << '\n';
 }
+
+
